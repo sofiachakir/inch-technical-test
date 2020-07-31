397a44a0bb7a3fd9218b4aea4577ed931ec9c866
sofiachakir <sofiachakir@gmail.com>
2020-07-31 11:03:59 +0200

    filter csv row input to prevent errors

--- app/models/person.rb
+++ app/models/person.rb
@@ -16,17 +16,35 @@ class Person < ApplicationRecord
 		response = []
 		counter = 1
 		CSV.foreach(file.path, headers: true) do |row|
-			# Convertir la ligne en hash
-			row_hash = row.to_h
-			# Vérifier l'existence de la référence
-			person = Person.find_by(reference: row_hash['reference'])
+			# Sélectionner les paramètres pour create / update
+			person_params = person_params(row)
+			# Si erreur, on quitte la boucle et on informe qu'il y a une erreur
+			if person_params.empty?
+				response << "CSV Format Error"
+				break
+			end
+			# Vérifier l'existence de la personne via sa référence
+			person = Person.find_by(reference: person_params['reference'])
 			# Si la personne existe, la mettre à jour, sinon, la créer
-			person ? person.update(row_hash) : person = Person.create(row_hash)
+			person ? person.update(person_params) : person = Person.create(person_params)
+			# Si on rencontre des erreurs, les enregistrer dans l'array response
 			response << "l#{counter} - #{person.errors.full_messages.join(',')}" if person.errors.any?
+			# Incrémenter le compteur
 			counter += 1
 		end
 		response.empty? ? response.push('Import was successful') : response.unshift('Errors prevented import')
 		return response
+	end
+
+	private
+
+	def self.person_params(row)
+		res = row.to_h.slice('reference', 'email', 'firstname', 'lastname', 'home_phone_number', 'mobile_phone_number', 'address')
+		if res.keys != ['reference', 'email', 'firstname', 'lastname', 'home_phone_number', 'mobile_phone_number', 'address']
+			return {}
+		else
+			return res
+		end
 	end
 
 end
